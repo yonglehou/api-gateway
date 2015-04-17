cf83bb56b58bded4956c8dcf685de9994d63dcf2
Damon Snyder <drsnyder@gmail.com>
2015-04-17 16:44:42 -0700

    Proxy all service requests to the load balancers.

--- templates/src/config.lua
+++ templates/src/config.lua
@@ -1,6 +1,7 @@
 -- templatized configuration: DO NOT EDIT
 --
 local config = {
+  SERVICE_LB_URL = "{{key "config/api-gateway/SERVICE_LB_URL"}}",
   HELIOS_URL = "{{key "config/api-gateway/HELIOS_URL"}}",
   SERVICE_HTTP_TIMEOUT = 0.1,
 }
