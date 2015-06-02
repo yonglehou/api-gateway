68dda28f49c5cd85a91965dad553bd5da99f8e61
Pawel Chojnacki <pawel@chojnacki.ws>
2015-06-02 21:09:39 +0200

    Support upstream servers in dynamic consul configured locations

---
+++ src/upstream.lua
@@ -0,0 +1,26 @@
+local util = require("util")
+
+local upstream = {
+  default = require("default-locations"),
+  configured = require("configured-locations"),
+}
+
+function upstream.find(request_uri) 
+  local first_url_token = util.get_url_prefix(request_uri)
+  
+  --find appropriate backend
+  local target = upstream.default[first_url_token]
+
+  --fallback to uri configured in Consul K/V
+  if target == nil or target == "" then
+    target = upstream.configured[first_url_token]
+  end
+
+  --set fake upstream name in case is not found
+  if target == nil or target == "" then
+    target = "__not_found"
+  end
+  return target
+end
+
+return upstream
