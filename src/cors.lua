54d5cd6734920ffcd65bca986ed04ddf54dd98d4
Pawel Chojnacki <pawel@chojnacki.ws>
2015-06-05 15:36:08 +0200

    add cors validation helper

--- src/cors.lua
+++ src/cors.lua
@@ -1,18 +1,19 @@
 local cors = {}
 
 local whitelist = {
-  ".*\\.wikia.com$",
-  "wikia.com$",
-  ".*\\.wikia-dev.com",
-  "wikia-dev.com$",
+  "^.*%.wikia%.com$",
+  "^wikia%.com$",
+  "^.*%.wikia%-dev%.com$",
+  "^wikia%-dev%.com$",
 }
 
 function cors.origin_matches_whitelist(origin_value) 
   if origin_value == nil then
     return false
   end
+  stripped = string.gsub(origin_value, "^https?://", "")
   for i, pattern in ipairs(whitelist) do
-    if string.match(origin_value, pattern) ~= nil then
+    if string.match(stripped, pattern) ~= nil then
       return true
     end
   end
