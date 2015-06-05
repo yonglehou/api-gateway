efb079ea13b5e0c0f5b4804d8071a30449733bfb
Pawel Chojnacki <pawel@chojnacki.ws>
2015-06-05 16:41:48 +0200

    whitelist based setter with tests

--- spec/cors_spec.lua
+++ spec/cors_spec.lua
@@ -1,5 +1,5 @@
 local cors = require "cors"
-describe("test CORS header helpers", function()
+describe("Validate whitelist checking method", function()
   it("domain whitelist detect proper origin domains", function()
     assert.are.same(true, cors.origin_matches_whitelist("http://wikia.com"))
     assert.are.same(true, cors.origin_matches_whitelist("http://stargate.test.wikia.com"))
@@ -15,5 +15,48 @@ describe("test CORS header helpers", function()
     assert.are.same(false, cors.origin_matches_whitelist("http://www.wikia.com.fakewebsite"))
     assert.are.same(false, cors.origin_matches_whitelist("http2://wikia.com"))
     assert.are.same(false, cors.origin_matches_whitelist("gopher://wikia.com"))
+  end)  
+end)
+
+describe("Response header handling", function()
+  -- args are passed to supplied method
+  function ngx_headers_allow_origin_and_origin(assert_fn, origin, response_headers, ngx_modifying_fn, ...)
+    ngx = {
+      req = {
+        get_headers = function(...)
+          local rv = {}
+          rv[cors.origin_header] = origin
+          return rv
+        end,
+      },
+      header = response_headers,
+    }
+    ngx_modifying_fn(ngx, unpack({...}))
+
+    assert_fn(origin, ngx.header[cors.allow_origin_header])
+  end
+
+  it("Response header allow origin is set correctly when origin matches whitelist", function()    
+    ngx_headers_allow_origin_and_origin(assert.are.equal, "http://wikia.com", {}, cors.set_whitelisted_allow_origin_header, true)
+    ngx_headers_allow_origin_and_origin(assert.are.equal, "http://wikia.com", {}, cors.set_whitelisted_allow_origin_header, false)
+
+    ngx_headers_allow_origin_and_origin(assert.are.equal, "https://stargate.test.wikia-dev.com", {}, cors.set_whitelisted_allow_origin_header, false)
+  end)
+
+  it("Response header allow origin is not set when origin doesn't matche the whitelist", function()    
+    ngx_headers_allow_origin_and_origin(assert.are_not.equal, "http://fakewikia.com", {}, cors.set_whitelisted_allow_origin_header, false)
+  end)
+
+  describe("When Access-Control-Allow-Origin header alread exists", function()
+    local existing_headers = {}
+    existing_headers[cors.allow_origin_header] = "https://something.else"
+
+    it("Response header allow origin is not overriden when override is not set", function()
+      ngx_headers_allow_origin_and_origin(assert.are_not.equal, "http://wikia.com", existing_headers, cors.set_whitelisted_allow_origin_header, false)
+    end)
+
+    it("Response header allow origin is overriden when override is set", function()
+      ngx_headers_allow_origin_and_origin(assert.are.equal, "http://wikia.com", existing_headers, cors.set_whitelisted_allow_origin_header, true)
+    end)
   end)  
 end)
