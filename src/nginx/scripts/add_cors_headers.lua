2aacc548fc05b7e8c49c1fc1838ddb5a5c04fa5d
Pawel Chojnacki <pawel@chojnacki.ws>
2015-06-05 17:50:11 +0200

    first version of cors header filter

--- src/nginx/scripts/add_cors_headers.lua
+++ src/nginx/scripts/add_cors_headers.lua
@@ -1,26 +1,6 @@
 local cors = require("cors")
-local origin = "Origin"
--- set Vary on Origin
-local old_header = ngx.header.Vary
-local target_header
-if old_header ~= nil && old_header ~= "" then
-  target_header = old_header + "," + origin
-else
-  target_header = origin
-end
-ngx.header.Vary = target_header
 
+cors.add_origin_to_vary_header(ngx)
 
--- mirror CORS header
-local cors_header_name = 'Access-Control-Allow-Origin'
-
--- check if already set then don't override
-if ngx.header[cors_header_name] == nil then
-  local req_headers = ngx.req.get_headers(20)
-  local origin_value = req_headers[origin]
-
-  if cors.origin_matched_whitelist(origin_value)      
-    ngx.header[cors_header_name] = origin_value
-  end
-end
-
+-- set or override allow header 
+cors.set_whitelisted_allow_origin_header(ngx, true)
