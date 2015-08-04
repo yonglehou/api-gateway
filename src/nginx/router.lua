bfee836b6ef637897c61466154b49746ec94f670
Damon Snyder <drsnyder@gmail.com>
2015-08-04 12:22:33 -0700

    Make /helios & /auth synonymous
    Merge /helios and & /auth under one route

---
+++ src/nginx/router.lua
@@ -0,0 +1,17 @@
+-- package router: A wrapper for routing requests
+
+local router = {}
+
+local config = require "config"
+local nginx = require "nginx"
+
+function router.route()
+  local app = nginx.init(config)
+
+  local headers = ngx.req.get_headers(100)
+  local user_id = nginx.authenticate(app, headers)
+  return nginx.service_proxy(ngx, user_id)
+end
+
+
+return router
