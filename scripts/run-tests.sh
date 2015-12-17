95c1110756d2d6c5223fdece8bde857ea446dcb2
Damon Snyder <drsnyder@gmail.com>
2015-12-16 20:57:19 -0800

    Fix the path to nginx

--- scripts/run-tests.sh
+++ scripts/run-tests.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-export PATH=$PATH:$HOME/.luarocks/bin:$HOME/local/bin
+export PATH=$PATH:$HOME/.luarocks/bin:$HOME/local/nginx/sbin
 export TEST_NGINX_BINARY=`which openresty`
 busted spec && prove -r t
